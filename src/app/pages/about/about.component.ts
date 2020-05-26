5ac69d46ae9e5e471610fcafe94aa4b11d1218ed
Aaron Anton <a3b.anton@gmail.com>
2020-05-26 14:05:07 -0400

    Seccion5

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MainDataService } from '../../services/main-data.service';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _mainData: MainDataService ) { } //se inyecta el servicio MainDataService en la propiedad _mainData
 
   ngOnInit(): void {
   }
--- src/app/services/main-data.service.ts
+++ src/app/services/main-data.service.ts
@@ -7,16 +7,22 @@ import { HttpClient } from '@angular/common/http'; // se importa HttpClientModul
 export class MainDataService {
 
   info:any = {};  //para llamarlo en el html
+  data:any = {};
   cargada = false; //no estoy claro por que
 
   constructor( private http: HttpClient ) { // se importa HttpClientModule en el servicio y en el app.module.ts
 
+    this.cargarInfo();   //se cargan las dos funciones creadas abajo para hacer las consultas http
+    this.cargarEquipo();
+  }
+  private cargarInfo()  {
+
     this.http.get('assets/data/main-data.json')
-      .subscribe( resp => {
+      .subscribe( resp =>  {
         this.info = resp;
-        this.cargada = true;
-        //console.log(resp);
-        console.log("main data .OK!");
+        //this.cargada = true;
+        console.log("cargarEquipo .OK!");
+        console.log(resp);
 
       // .subscribe( (resp:any) => {
       // console.log(resp.tw);
@@ -24,6 +30,15 @@ export class MainDataService {
       // .subscribe( resp => {
       //   console.log(resp['tw']);
       });
-    
+  }
+  private cargarEquipo()  {
+
+    this.http.get('https://angular-portafolio-2c2e6.firebaseio.com/.json')
+      .subscribe( resp =>  {
+        this.data = resp;
+        console.log("cargarData .OK!");
+        console.log(resp);
+        
+      });
   }
 }
